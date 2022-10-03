8846c5b4eac72f5d4637ec37547906ed20e5bd23
onyanko-pon <engineer.tsukasa@gmail.com>
2022-10-03 23:22:49 +0900

    filter notification

--- app/main.go
+++ app/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"embed"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/onyanko-pon/scheduled_slack_notification/app/internal/setting"
 	"github.com/onyanko-pon/scheduled_slack_notification/app/internal/usecase"
@@ -21,9 +23,28 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	sntf := setting.Get().Notifications[0]
+
+	if len(os.Args) < 2 {
+		log.Fatal(fmt.Errorf("notification not selected"))
+	}
+
+	ntfName := os.Args[1]
+	sntf, err := filterNotification(setting.Get().Notifications, ntfName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	err = us.Do(ctx, sntf)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func filterNotification(ntfs []setting.Notification, name string) (setting.Notification, error) {
+	for _, ntf := range ntfs {
+		if ntf.Name == name {
+			return ntf, nil
+		}
+	}
+	return setting.Notification{}, fmt.Errorf("notification: %s is not found", name)
 }
