4a85f24017144994830739bb00317871a219791e
onyanko-pon <engineer.tsukasa@gmail.com>
2022-10-06 05:56:35 +0900

    add: gcloud option

--- terraform/modules/runjob/main.tf
+++ terraform/modules/runjob/main.tf
@@ -11,7 +11,7 @@ module "gcloud" {
   additional_components = ["beta"]
 
   create_cmd_entrypoint  = "gcloud"
-  create_cmd_body        = "beta run jobs create ${var.jobname} --image=${var.image_url} --set-env-vars=${local.env_vars_str} --set-secrets=${local.secrets_str} --project ${var.project} --region ${var.region}"
+  create_cmd_body        = "beta run jobs create ${var.jobname} --image=${var.image_url} --set-env-vars=${local.env_vars_str} --set-secrets=${local.secrets_str} --project ${var.project} --region ${var.region} --quiet"
   destroy_cmd_entrypoint = "gcloud"
   destroy_cmd_body       = "beta run jobs delete ${var.jobname} --project ${var.project} --region ${var.region} --quiet"
 }
